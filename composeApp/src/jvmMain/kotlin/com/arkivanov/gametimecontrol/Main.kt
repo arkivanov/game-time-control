87d86f16cf90d5eb992701a4e76fe0aed837adc3
Arkadii Ivanov <arkann1985@gmail.com>
2024-12-14 18:04:07 +0000

    Added tray icon

--- composeApp/src/jvmMain/kotlin/com/arkivanov/gametimecontrol/Main.kt
+++ composeApp/src/jvmMain/kotlin/com/arkivanov/gametimecontrol/Main.kt
@@ -1,19 +1,40 @@
 package com.arkivanov.gametimecontrol
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberTrayState
 import androidx.compose.ui.window.rememberWindowState
 import com.arkivanov.gametimecontrol.theme.AppTheme
 import java.awt.Dimension
 
+
 fun main() {
     application {
+        val windowState = rememberWindowState(width = 800.dp, height = 600.dp)
+        val trayState = rememberTrayState()
+
+        Tray(
+            icon = rememberVectorPainter(Icons.Default.Lock),
+            state = trayState,
+            onAction = { windowState.isMinimized = false },
+            menu = {
+                Item(
+                    text = "Game Time Control",
+                    onClick = { windowState.isMinimized = false },
+                )
+            },
+        )
+
         Window(
             title = "GameTimeControl",
-            state = rememberWindowState(width = 800.dp, height = 600.dp),
+            state = windowState,
             onCloseRequest = ::exitApplication,
         ) {
             window.minimumSize = Dimension(350, 600)
